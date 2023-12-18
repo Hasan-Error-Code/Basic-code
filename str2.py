3d565cb8d60f263fd8bc69120d7d5698d446ec63
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-18 23:15:06 +0600

    string

---
+++ str2.py
@@ -0,0 +1,25 @@
+country = "Bangladesh"
+f = country.find("Ban")
+print(f)
+
+print("-" * 50)
+
+coun = "North Corea"
+count = coun.replace("North", "South")
+print(count)
+print(coun)
+print("-" * 50)
+
+text = "hello"
+text = text.replace("h", "H")
+print(text)
+print("-" * 50)
+
+st = "  Hey there this  is Hasan  "
+mod = st.lstrip()
+print(mod)
+mod2 = st.rstrip()
+print(mod2)
+mod3 = st.strip()
+print(mod3)
+print("-" * 50)
